4272e2a679847526ed4987ed0eeb73301dc37c41
Ivan <chepiga@rozetka.com.ua>
2017-03-17 18:22:40 +0200

    update v2

--- my-app/src/app/my-comment/my-comment.component.ts
+++ my-app/src/app/my-comment/my-comment.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-my-comment',
@@ -8,22 +8,25 @@ import { Component, OnInit, Input } from '@angular/core';
 
 export class MyCommentComponent {
 
-Comments = [
-  { id: 1, text : 'text 1', mark : 1000 },
-  { id: 2, text : 'text 2', mark : 0 },
-  { id: 3, text : 'text 3', mark : 5 },
-  { id: 4, text : 'text 4', mark : 3 },
-  { id: 5, text : 'text 5', mark : 4 },
-  { id: 6, text : 'text 6', mark : null },
-  { id: 7, text : 'text 7', mark : 5 },
-  { id: 8, text : 'text 8', mark : 2 },
-  { id: 9, text : 'text 9', mark : 1 },
-  { id: 10, text : 'text 10', mark : 4 },
-];
+  Comments = [
+    { id: 1, text : 'text 1', mark : 1000 },
+    { id: 2, text : 'text 2', mark : 0 },
+    { id: 3, text : 'text 3', mark : 5 },
+    { id: 4, text : 'text 4', mark : 3 },
+    { id: 5, text : 'text 5', mark : 4 },
+    { id: 6, text : 'text 6', mark : null },
+    { id: 7, text : 'text 7', mark : 5 },
+    { id: 8, text : 'text 8', mark : 2 },
+    { id: 9, text : 'text 9', mark : 1 },
+    { id: 10, text : 'text 10', mark : 4 },
+  ];
 
-@Input()
-rows: number = 0;
+  @Input()
+  rows: number = 0;
 
-delete_row() {
-}
+  delete_row (i) {
+    this.Comments.splice(i, 1);
+    this.rows = this.rows -1;
+    // console.log(this.Comments.values);
+  }
 }
